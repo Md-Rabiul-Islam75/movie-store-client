5fdcfcdbfb1d54e9b5f183c5f4973b875dc4cb2a
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-08-12 16:25:49 +0600

    add something in aboutUs page

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import firstPic from "../assets/aboutMovie1.avif"
+import secondPic from "../assets/aboutMovie2.avif"
 
 const AboutUsPage = () => {
   return (
@@ -7,25 +9,25 @@ const AboutUsPage = () => {
 
       <div className="shadow-xl">
         <h3>Our Goal</h3>
-        <div>
-          <img src="" alt="" />
-          <p></p>
+        <div className="flex flex-col md:flex-row gap-2">
+          <img src={firstPic} alt="" />
+          <p>Storing all kinds of movies in one place.</p>
         </div>
       </div>
 
       <div className="shadow-xl">
         <h3>Our Mission</h3>
-        <div>
-          <img src="" alt="" />
-          <p></p>
+        <div className="flex flex-col md:flex-row gap-2">
+          <img src={secondPic} alt="" />
+          <p>Centralize all movie. User can add movie. Select favourite movie, update movie etc.</p>
         </div>
       </div>
 
       <div className="shadow-xl">
         <h3>Our Vision</h3>
-        <div>
-          <img src="" alt="" />
-          <p></p>
+        <div className="flex flex-col md:flex-row gap-2">
+          <img src={firstPic} alt="" />
+          <p>Give all kinds of support in one website.</p>
         </div>
       </div>
     </div>
